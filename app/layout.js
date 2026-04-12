5aef41a192b2034f28a35963f7317002a8a9b8ef
Your Name <your-email@example.com>
2026-04-12 12:00:37 +0545

    ui changes

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="description" content={metadata.description} />
+      </head>
       <body>
         <UiProvider>
           <Navbar />
